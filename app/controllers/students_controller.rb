908f7c583c30d0856d84983dae35e3414c0b2397
collo <mutaic84@gmail.com>
2022-07-18 23:24:26 +0300

    Route params lab code solution

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,19 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
+
+    students = if params[:name]
+                  Student.find_by(first_name: params[:name]) || Student.find_by(last_name: params[:name])
+              else
+                Student.all
+              end
     render json: students
+    # byebug
+  end
+
+  def show
+    student = Student.find(params[:id])
+    render json: student
   end
 
 end
