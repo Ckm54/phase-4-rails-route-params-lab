0684c0ab2c771175419dbf13641255ff18dc8e26
collo <mutaic84@gmail.com>
2022-07-19 15:12:05 +0300

    update search with prams

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -3,7 +3,7 @@ class StudentsController < ApplicationController
   def index
 
     students = if params[:name]
-                  Student.find_by(first_name: params[:name]) || Student.find_by(last_name: params[:name])
+                  Student.find_by(first_name: params[:name].downcase.titleize) || Student.find_by(last_name: params[:name].downcase.titleize)
               else
                 Student.all
               end
